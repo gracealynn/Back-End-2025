d567d845fd3cfb61b5e6d726624f2b2ac40aa92c
gracealynn <s22210508@student.unklab.ac.id>
2025-02-19 22:51:32 +0800

    Exercise 4

--- index.js
+++ index.js
@@ -6,18 +6,37 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("This is home page"));
-app.get("/about", (req, res) =>
-  res.status(200).json({
-    status: "success",
-    message: "response success",
-    description: "Exercise 3",
-    date: moment().format("MMMM Do YYYY, h:mm:ss a"),
-  })
-);
-
 app.get("/users", (req, res) => {
   res.send(users);
 });
+
+app.get("/users/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  const user = users.find((data) => data.name.toLowerCase() === name);
+  if (!user) {
+    return res.status(404).json({
+      message: "Data user tidak ditemukan",
+    });
+  }
+
+  res.status(200).json({ user });
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "error",
+    message: "resource tidak ditemukan",
+  });
+});
+
+const errorhandling = (err, req, res, next) => {
+  res.json({
+    status: "error",
+    message: "terjadi kesalahan pada server",
+  });
+};
+
+app.use(errorhandling);
 
 const hostname = "127.0.0.1";
 const port = 3000;
